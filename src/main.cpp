9ddc6ce7326e45f79a7ccbe4a181afe80b22c4fe
Alan Machado <alanpinheirorj22@gmail.com>
2026-07-20 17:40:25 -0300

    feat: crate progress_bar

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 #include <chrono>
+#include <string>
 #include <thread>
+#include <clocale>
 
 using namespace std;
 int main(){
+    std::setlocale(LC_ALL, "");
+    
     int time = 1500;
     while(time > 0){
         
         int minutes = time/60;
         int seconds = time % 60;
+        int elapsed_time = 1500 - time;
+        int progress = elapsed_time * 100 / 1500; 
+            
         cout << minutes << ":"; 
 
         if(seconds < 10){
             cout << "0";
         }
 
-        cout <<seconds<< "\r" << flush;
+        cout <<seconds << "\n";
+
+        std::string progress_bar = "";
+        for(int i = 0; i <= progress; i ++){
+            progress_bar += "█";
+        }
+
+        std::string empty_bar = "";
+        for(int i = 0; i <= 100; i++){
+            empty_bar += "░";
+        }
+        cout << progress_bar << empty_bar << " "<< progress << "%" << "\033[K" << "\n";
+        cout << "\033[2A" << flush;
         this_thread::sleep_for(chrono::seconds(1));
         time = time - 1; 
     }
